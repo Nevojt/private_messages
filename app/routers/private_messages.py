ce36d5c51b9d86289acae119b2b03015802690b0
Nevojt <nevojt@gmail.com>
2024-07-11 20:44:53 +0200

    marking not working

--- app/routers/private_messages.py
+++ app/routers/private_messages.py
@@ -142,7 +142,7 @@ async def web_private_endpoint(
                                     avatar=user.avatar,
                                     verified=user.verified,
                                     id_return=original_message_id,
-                                    is_read=not is_recipient_active
+                                    is_read=True     # not is_recipient_active
                                     )
                                             
     except WebSocketDisconnect:
